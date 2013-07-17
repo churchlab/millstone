2eff9e437b16c57cc76338af74014cb65ea61e9e
glebk <gleb.kuznetsov@gmail.com>
2013-07-17 16:55:56 -0400

    Create static dir. Add latest bootstrap.

--- genome_designer/settings.py
+++ genome_designer/settings.py
@@ -123,10 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'main'
 )
 
 # A sample logging configuration. The only tangible logging
