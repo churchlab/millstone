b484d7b43d93d43dfbdce4955101395845ef448b
glebk <gleb.kuznetsov@gmail.com>
2013-07-22 16:07:10 -0400

    Get rid of the logging section of settings.py which actually doesn't do anything, but is causing a deprecation warning.

--- genome_designer/settings.py
+++ genome_designer/settings.py
@@ -125,26 +125,3 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'main'
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
