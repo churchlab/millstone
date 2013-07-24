dae5da6e29ecb806ae505f904175cf962b59ae57
glebk <gleb.kuznetsov@gmail.com>
2013-07-24 17:05:06 -0400

    First demonstration of the Datatables javascript component. Just using hard-coded data for now.

--- genome_designer/main/static/js/variant_view.js
+++ genome_designer/main/static/js/variant_view.js
@@ -1,16 +1,64 @@
 /**
- * @fileoverview List of alignments.
+ * @fileoverview List of variants.
  */
 
 
 gd.VariantView = Backbone.View.extend({
   el: '#gd-page-container',
 
+
   initialize: function() {
     this.render();
+
+    this.updateDatatable();
   },
+
 
   render: function() {
     $('#gd-sidenav-link-variants').addClass('active');
+  },
+
+
+  /** Update the datatable. */
+  updateDatatable: function() {
+    $('#gd-datatable-hook').html(
+        '<table cellpadding="0" cellspacing="0" border="0" class="display"' +
+            'id="example">' +
+        '</table>');
+    $('#example').dataTable( {
+        "aaData": [
+            /* Reduced data set */
+            [ "Trident", "Internet Explorer 4.0", "Win 95+", 4, "X" ],
+            [ "Trident", "Internet Explorer 5.0", "Win 95+", 5, "C" ],
+            [ "Trident", "Internet Explorer 5.5", "Win 95+", 5.5, "A" ],
+            [ "Trident", "Internet Explorer 6.0", "Win 98+", 6, "A" ],
+            [ "Trident", "Internet Explorer 7.0", "Win XP SP2+", 7, "A" ],
+            [ "Gecko", "Firefox 1.5", "Win 98+ / OSX.2+", 1.8, "A" ],
+            [ "Gecko", "Firefox 2", "Win 98+ / OSX.2+", 1.8, "A" ],
+            [ "Gecko", "Firefox 3", "Win 2k+ / OSX.3+", 1.9, "A" ],
+            [ "Webkit", "Safari 1.2", "OSX.3", 125.5, "A" ],
+            [ "Webkit", "Safari 1.3", "OSX.3", 312.8, "A" ],
+            [ "Webkit", "Safari 2.0", "OSX.4+", 419.3, "A" ],
+            [ "Webkit", "Safari 3.0", "OSX.4+", 522.1, "A" ],
+            [ "Trident", "Internet Explorer 4.0", "Win 95+", 4, "X" ],
+            [ "Trident", "Internet Explorer 5.0", "Win 95+", 5, "C" ],
+            [ "Trident", "Internet Explorer 5.5", "Win 95+", 5.5, "A" ],
+            [ "Trident", "Internet Explorer 6.0", "Win 98+", 6, "A" ],
+            [ "Trident", "Internet Explorer 7.0", "Win XP SP2+", 7, "A" ],
+            [ "Gecko", "Firefox 1.5", "Win 98+ / OSX.2+", 1.8, "A" ],
+            [ "Gecko", "Firefox 2", "Win 98+ / OSX.2+", 1.8, "A" ],
+            [ "Gecko", "Firefox 3", "Win 2k+ / OSX.3+", 1.9, "A" ],
+            [ "Webkit", "Safari 1.2", "OSX.3", 125.5, "A" ],
+            [ "Webkit", "Safari 1.3", "OSX.3", 312.8, "A" ],
+            [ "Webkit", "Safari 2.0", "OSX.4+", 419.3, "A" ],
+        ],
+        "aoColumns": [
+            { "sTitle": "Engine" },
+            { "sTitle": "Browser" },
+            { "sTitle": "Platform" },
+            { "sTitle": "Version", "sClass": "center" },
+            { "sTitle": "Grade", "sClass": "center" }
+        ]
+    });
   }
 });
