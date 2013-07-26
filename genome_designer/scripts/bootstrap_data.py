c946fae269633eacc51987baf1c5a895f6906982
glebk <gleb.kuznetsov@gmail.com>
2013-07-26 18:06:30 -0400

    Speed up bootstrap_data.py by wrapping the script in an @transaction.commit_on_success block to condense all the requests to the SQL db.

--- genome_designer/scripts/bootstrap_data.py
+++ genome_designer/scripts/bootstrap_data.py
@@ -21,87 +21,90 @@ PWD = os.path.dirname(os.path.realpath(__file__ ))
 def bootstrap_fake_data():
     """Fill the database with fake data.
     """
+    from django.db import transaction
 
-    # Imports only work after the environment has been set up.
-    from django.contrib.auth.models import User
+    @transaction.commit_on_success
+    def transaction_wrapper():
+        # Imports only work after the environment has been set up.
+        from django.contrib.auth.models import User
 
-    ### Get or create the user.
-    TEST_USERNAME = 'gmcdev'
-    TEST_PASSWORD = 'g3n3d3z'
-    TEST_EMAIL = 'gmcdev@genomedesigner.freelogy.org'
-    try:
-        user = User.objects.get(username=TEST_USERNAME)
-    except User.DoesNotExist:
-        user = User.objects.create_user(
-                TEST_USERNAME, password=TEST_PASSWORD, email=TEST_EMAIL)
+        ### Get or create the user.
+        TEST_USERNAME = 'gmcdev'
+        TEST_PASSWORD = 'g3n3d3z'
+        TEST_EMAIL = 'gmcdev@genomedesigner.freelogy.org'
+        try:
+            user = User.objects.get(username=TEST_USERNAME)
+        except User.DoesNotExist:
+            user = User.objects.create_user(
+                    TEST_USERNAME, password=TEST_PASSWORD, email=TEST_EMAIL)
 
-    ### Create some projects
-    from main.models import Project
-    TEST_PROJECT_NAME = 'recoli'
-    (test_project, project_created) = Project.objects.get_or_create(
-            title=TEST_PROJECT_NAME, owner=user.get_profile())
-    (test_project_2, project_created) = Project.objects.get_or_create(
-            title='project2', owner=user.get_profile())
-    (test_project_3, project_created) = Project.objects.get_or_create(
-            title='project3', owner=user.get_profile())
+        ### Create some projects
+        from main.models import Project
+        TEST_PROJECT_NAME = 'recoli'
+        (test_project, project_created) = Project.objects.get_or_create(
+                title=TEST_PROJECT_NAME, owner=user.get_profile())
+        (test_project_2, project_created) = Project.objects.get_or_create(
+                title='project2', owner=user.get_profile())
+        (test_project_3, project_created) = Project.objects.get_or_create(
+                title='project3', owner=user.get_profile())
 
-    ### Create some reference genomes
-    from main.models import ReferenceGenome
-    REF_GENOME_1_LABEL = 'mg1655'
-    (ref_genome_1, ref_genome_created) = ReferenceGenome.objects.get_or_create(
-            label=REF_GENOME_1_LABEL, project=test_project, num_chromosomes=1,
-            num_bases=100)
-    REF_GENOME_2_LABEL = 'c321D'
-    (ref_genome_2, ref_genome_created) = ReferenceGenome.objects.get_or_create(
-            label=REF_GENOME_2_LABEL, project=test_project, num_chromosomes=1,
-            num_bases=200)
+        ### Create some reference genomes
+        from main.models import ReferenceGenome
+        REF_GENOME_1_LABEL = 'mg1655'
+        (ref_genome_1, ref_genome_created) = ReferenceGenome.objects.get_or_create(
+                label=REF_GENOME_1_LABEL, project=test_project, num_chromosomes=1,
+                num_bases=100)
+        REF_GENOME_2_LABEL = 'c321D'
+        (ref_genome_2, ref_genome_created) = ReferenceGenome.objects.get_or_create(
+                label=REF_GENOME_2_LABEL, project=test_project, num_chromosomes=1,
+                num_bases=200)
 
-    ### Create some samples
-    from main.models import ExperimentSample
-    SAMPLE_1_LABEL = 'sample1'
-    (sample_1, created) = ExperimentSample.objects.get_or_create(
-            project=test_project,
-            label=SAMPLE_1_LABEL)
+        ### Create some samples
+        from main.models import ExperimentSample
+        SAMPLE_1_LABEL = 'sample1'
+        (sample_1, created) = ExperimentSample.objects.get_or_create(
+                project=test_project,
+                label=SAMPLE_1_LABEL)
 
-    ### Add datasets to the samples.
-    from main.models import Dataset
+        ### Add datasets to the samples.
+        from main.models import Dataset
 
-    dataset_1 = Dataset.objects.create(
-            type=Dataset.TYPE.FASTQ1,
-            label='sample1_fastq1',
-            filesystem_location='blah')
-    sample_1.dataset_set.add(dataset_1)
+        dataset_1 = Dataset.objects.create(
+                type=Dataset.TYPE.FASTQ1,
+                label='sample1_fastq1',
+                filesystem_location='blah')
+        sample_1.dataset_set.add(dataset_1)
 
-    dataset_2 = Dataset.objects.create(
-            type=Dataset.TYPE.FASTQ2,
-            label='sample1_fastq2',
-            filesystem_location='blah2')
-    sample_1.dataset_set.add(dataset_2)
+        dataset_2 = Dataset.objects.create(
+                type=Dataset.TYPE.FASTQ2,
+                label='sample1_fastq2',
+                filesystem_location='blah2')
+        sample_1.dataset_set.add(dataset_2)
 
-    ### Create an alignment.
-    from main.models import AlignmentGroup
-    alignment_group_1 = AlignmentGroup.objects.create(
-            label='Alignment 1',
-            reference_genome=ref_genome_1,
-            aligner=AlignmentGroup.ALIGNER.BWA)
+        ### Create an alignment.
+        from main.models import AlignmentGroup
+        alignment_group_1 = AlignmentGroup.objects.create(
+                label='Alignment 1',
+                reference_genome=ref_genome_1,
+                aligner=AlignmentGroup.ALIGNER.BWA)
 
-    # Link it to a sample.
-    from main.models import ExperimentSampleToAlignment
-    ExperimentSampleToAlignment.objects.create(
-            alignment_group=alignment_group_1,
-            experiment_sample=sample_1)
+        # Link it to a sample.
+        from main.models import ExperimentSampleToAlignment
+        ExperimentSampleToAlignment.objects.create(
+                alignment_group=alignment_group_1,
+                experiment_sample=sample_1)
 
 
-    ### Create some fake variants
-    from main.models import Variant
-    for var_count in range(100):
-        Variant.objects.create(
-            type=Variant.TYPE.TRANSITION,
-            reference_genome=ref_genome_1,
-            chromosome='chrom',
-            position=random.randint(1,ref_genome_1.num_bases),
-            ref_value='A',
-            alt_value='G')
+        ### Create some fake variants
+        from main.models import Variant
+        for var_count in range(100):
+            Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=ref_genome_1,
+                chromosome='chrom',
+                position=random.randint(1,ref_genome_1.num_bases),
+                ref_value='A',
+                alt_value='G')
 
 
 def reset_database():
