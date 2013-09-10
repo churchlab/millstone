1a8c4af490105fcfbe64ef8795db489a1b181a19
glebk <gleb.kuznetsov@gmail.com>
2013-09-10 18:03:30 -0400

    Working on Variant filtering - support multiple query clause for limit valid keys.
    This commit introduces our new symbolic strategy, moving towards getting
    to the DNF so that we can handle as many of the queries in SQL before
    moving on to manual key-value manipulation.

--- genome_designer/scripts/tests/test_variant_filter.py
+++ genome_designer/scripts/tests/test_variant_filter.py
@@ -4,14 +4,20 @@ Tests for variant_filter.py.
 
 from django.contrib.auth.models import User
 from django.test import TestCase
+from sympy.core.function import sympify
 
 from main.models import Project
 from main.models import ReferenceGenome
 from main.models import Variant
-
 from scripts.variant_filter import get_variants_that_pass_filter
+from scripts.variant_filter import symbol_generator
+from scripts.variant_filter import ParseError
+from scripts.variant_filter import VariantFilterEvaluator
 
-class TestVariantFilter(TestCase):
+
+class BaseTestVariantFilterTestCase(TestCase):
+    """Base class for the test classes here.
+    """
 
     def setUp(self):
         user = User.objects.create_user('testuser', password='password',
@@ -21,6 +27,8 @@ class TestVariantFilter(TestCase):
         self.ref_genome = ReferenceGenome.objects.create(project=project,
                 label='refgenome', num_chromosomes=1, num_bases=1000)
 
+
+class TestVariantFilter(BaseTestVariantFilterTestCase):
 
     def test_filter__by_position(self):
         """Test filtering by position.
@@ -79,3 +87,87 @@ class TestVariantFilter(TestCase):
 
         self.assertEqual(9, len(get_variants_that_pass_filter(
                 'chromosome = chrom2', self.ref_genome)))
+
+
+    def test_filter__by_position_and_chromosome(self):
+        """Test filtering by position.
+        """
+        CHROM_1 = 'chrom'
+        for pos in range(6):
+            Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_1,
+                position=pos,
+                ref_value='A',
+                alt_value='G')
+
+        CHROM_2 = 'chrom2'
+        for pos in range(9):
+            Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_2,
+                position=pos,
+                ref_value='A',
+                alt_value='G')
+
+        QUERY_STRING = 'position > 4 & chromosome = chrom'
+        self.assertEqual(1, len(get_variants_that_pass_filter(
+                QUERY_STRING, self.ref_genome)))
+
+        QUERY_STRING = 'position >= 5 & chromosome = chrom2'
+        self.assertEqual(4, len(get_variants_that_pass_filter(
+                QUERY_STRING, self.ref_genome)))
+
+
+    def test_filter__invalid_key(self):
+        CHROM_1 = 'chrom'
+        for pos in range(6):
+            Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_1,
+                position=pos,
+                ref_value='A',
+                alt_value='G')
+
+        QUERY_STRING = 'dinosaur > 4 & chromosome = chrom'
+        with self.assertRaises(ParseError):
+            get_variants_that_pass_filter(QUERY_STRING, self.ref_genome)
+
+
+
+class TestVariantFilterEvaluator(BaseTestVariantFilterTestCase):
+    """Tests for the object that encapsulates evaluation of the filter string.
+    """
+
+    def test_variant_filter_constructor(self):
+        """Tests the constructor.
+        """
+        raw_filter_string = 'position > 5'
+        evaluator = VariantFilterEvaluator(raw_filter_string, self.ref_genome)
+        EXPECTED_SYMBOLIC_REP = sympify('A')
+        self.assertEqual(EXPECTED_SYMBOLIC_REP, evaluator.sympy_representation)
+        self.assertEqual('position>5',
+                evaluator.symbol_to_expression_map['A'])
+
+        raw_filter_string = 'position>5 & chromosome= chrom1'
+        evaluator = VariantFilterEvaluator(raw_filter_string, self.ref_genome)
+        EXPECTED_SYMBOLIC_REP = sympify('A & B')
+        self.assertEqual(EXPECTED_SYMBOLIC_REP, evaluator.sympy_representation)
+        self.assertEqual('position>5',
+                evaluator.symbol_to_expression_map['A'])
+        self.assertEqual('chromosome=chrom1',
+                evaluator.symbol_to_expression_map['B'])
+
+
+class TestSymbolGenerator(TestCase):
+    """Tests the symbol generator used for symbolic manipulation.
+    """
+
+    def test_generator(self):
+        symbol_maker = symbol_generator()
+        self.assertEqual('A', symbol_maker.next())
+        self.assertEqual('B', symbol_maker.next())
+        self.assertEqual('C', symbol_maker.next())
--- genome_designer/scripts/variant_filter.py
+++ genome_designer/scripts/variant_filter.py
@@ -3,8 +3,10 @@ Methods for filtering over Variants.
 """
 
 from collections import OrderedDict
+import re
 
 from django.db.models import Q
+from sympy.core.function import sympify
 
 
 ###############################################################################
@@ -41,36 +43,92 @@ DELIM_TO_Q_POSTFIX['<'] = '__lt'
 DELIM_TO_Q_POSTFIX['>'] = '__gt'
 DELIM_TO_Q_POSTFIX['='] = ''
 
+
 ###############################################################################
-# Main entry method.
+# Helper objects.
 ###############################################################################
 
-def get_variants_that_pass_filter(filter_string, ref_genome):
-    """Takes a complete filter string and returns the variants that pass the
-    filter.
+class ParseError(Exception):
+    """Exception raised for errors in the input.
 
-    We have a hybrid implementation for field values, where some of the field
-    values are actually columns in the respective database tables, while others
-    are serialized (pickled) as part of a single key-value string. We do
-    filtering over the key-value parts in-memory in the Django server, but on
-    a set of objects returned from the SQL database after filtering down by
-    the supported DB keys.
-
-    Args:
-        filter_string: Query string from the user.
-        ref_genome: The ReferenceGenome this is limited to. This is a hard-coded
-            parameter of sorts, since at the least we always limit comparisons
-            among Variant objects to those that share a ReferenceGenome.
-
-    Returns:
-        List of Variant model objects.
+    Attributes:
+        expr -- input expression in which the error occurred
+        msg  -- explanation of the error
     """
-    filter_string = filter_string.replace(' ', '')
-    delim_key_value_triple = _get_delim_key_value_triple(filter_string)
-    q_obj = _get_django_q_object_for_triple(delim_key_value_triple)
-    query_result = ref_genome.variant_set.filter(q_obj)
-    return query_result
 
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+    def __str__(self):
+        return ('Parse Error while parsing: ' + str(self.expr) + '. ' +
+                str(self.msg))
+
+
+def symbol_generator():
+    """Generates symbols for manipulation.
+    """
+    final_symbol_ord = ord('z')
+    current_char = 'A'
+    while True:
+        yield current_char
+        next_ord = ord(current_char) + 1
+        if next_ord > final_symbol_ord:
+            raise StopIteration()
+        current_char = chr(next_ord)
+
+
+EXPRESSION_REGEX = re.compile('(\w+[=><]{1}[=]{0,1}\w+)')
+
+class VariantFilterEvaluator(object):
+    """Object that encapsulates methods for evaluating a filter.
+    """
+
+    def __init__(self, raw_filter_string, ref_genome):
+        self.raw_filter_string = raw_filter_string
+        self.clean_filter_string = raw_filter_string.replace(' ', '')
+        self.ref_genome = ref_genome
+        self._create_symbolic_representation()
+
+
+    def _create_symbolic_representation(self):
+        """Creates a symbolic representation of the query to enable, among
+        other things, manipulation with sympy so we can get to disjunctive
+        normal form (DNF).
+        """
+        # Find all the expressions and replace them with symbols.
+        self.symbol_to_expression_map = {}
+
+        tokens = EXPRESSION_REGEX.split(self.clean_filter_string)
+        symbol_maker = symbol_generator()
+        # Iterate through tokens and replace them with successive symbols.
+        symbolified_tokens = []
+        for token in tokens:
+            if EXPRESSION_REGEX.match(token):
+                symbol = symbol_maker.next()
+                self.symbol_to_expression_map[symbol] = token
+                symbolified_tokens.append(symbol)
+            else:
+                symbolified_tokens.append(token)
+        symbolified_string = ''.join(symbolified_tokens)
+        self.sympy_representation = sympify(symbolified_string)
+
+
+    def get_condition_string_for_symbol(self, symbol):
+        if isinstance(symbol, str):
+            symbol_str = symbol
+        else:
+            symbol_str = str(symbol)
+        return self.symbol_to_expression_map[symbol_str]
+
+
+class QueryWrapper(object):
+    """Object that wraps a simple query, along with metadata depending on the
+    target model for the query.
+    """
+
+    def __init__(self, raw_query_string):
+        self.raw_query_string = raw_query_string
 
 ###############################################################################
 # Helper methods
@@ -124,19 +182,49 @@ def _get_django_q_object_for_triple(delim_key_value_triple):
 
 
 ###############################################################################
-# Helper objects (scratch for now).
+# Main entry method.
 ###############################################################################
 
-class QueryWrapper(object):
-    """Object that wraps a simple query, along with metadata depending on the
-    target model for the query.
+def get_variants_that_pass_filter(filter_string, ref_genome):
+    """Takes a complete filter string and returns the variants that pass the
+    filter.
+
+    We have a hybrid implementation for field values, where some of the field
+    values are actually columns in the respective database tables, while others
+    are serialized (pickled) as part of a single key-value string. We do
+    filtering over the key-value parts in-memory in the Django server, but on
+    a set of objects returned from the SQL database after filtering down by
+    the supported DB keys.
+
+    Args:
+        filter_string: Query string from the user.
+        ref_genome: The ReferenceGenome this is limited to. This is a hard-coded
+            parameter of sorts, since at the least we always limit comparisons
+            among Variant objects to those that share a ReferenceGenome.
+
+    Returns:
+        List of Variant model objects.
     """
+    evaluator = VariantFilterEvaluator(filter_string, ref_genome)
+    q_obj_list = []
+    if not evaluator.sympy_representation.is_Boolean:
+        condition_string = evaluator.get_condition_string_for_symbol(
+                evaluator.sympy_representation)
+        delim_key_value_triple = _get_delim_key_value_triple(condition_string)
+        q_obj_list.append(_get_django_q_object_for_triple(
+                delim_key_value_triple))
+    else:
+        for symbol in evaluator.sympy_representation.args:
+            condition_string = evaluator.get_condition_string_for_symbol(symbol)
+            delim_key_value_triple = _get_delim_key_value_triple(condition_string)
+            q_obj_list.append(_get_django_q_object_for_triple(
+                    delim_key_value_triple))
 
-    def __init__(self, raw_query_string):
-        self.raw_query_string = raw_query_string
+    # Combine the Q objects.
+    full_q = Q()
+    for q_obj in q_obj_list:
+        full_q &= q_obj
 
-
-class VariantFilterEvaluator(object):
-    """Object that encapsulates methods for evaluating a filter.
-    """
-    pass
+    # Send the query to the database.
+    query_result = ref_genome.variant_set.filter(full_q)
+    return query_result
