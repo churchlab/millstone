churchlab/millstone

commit f4f5852944d1fd1b9e96a70cb4496ee6e1e66dc0

Fix tests: Allow for celery not to be running when doing in-memory celery for tests.

genome_designer/main/celery_util.py
@@ -5,6 +5,7 @@ Methods for interfacing with the Celery task queue management library.
 from errno import errorcode
 
 from celery.task.control import inspect
+from django.conf import settings
 
 
 CELERY_ERROR_KEY = 'ERROR'
@@ -14,6 +15,10 @@ def get_celery_worker_status():
 
     Source: http://stackoverflow.com/questions/8506914/detect-whether-celery-is-available-running
     """
+    if settings.BROKER_BACKEND == 'memory':
+        # We are testing with in-memory celery. Celery is effectively running.
+        return {}
+
     try:
         insp = inspect()
         d = insp.stats()

commit 6b83f9f3a52a24f2c89237638f82ec70ace6a0c5

Fix bug with Celery jobs introduced in f4f5852944d1fd1b9e96a70cb4496ee6e1e66dc0.
When I fixed the issue with tests failing due to Celery not running,
I broke functionality of Celery when it is running.

genome_designer/main/celery_util.py
@@ -15,7 +15,7 @@ def get_celery_worker_status():
 
     Source: http://stackoverflow.com/questions/8506914/detect-whether-celery-is-available-running
     """
-    if settings.BROKER_BACKEND == 'memory':
+    if hasattr(settings, 'BROKER_BACKEND') and settings.BROKER_BACKEND == 'memory':
         # We are testing with in-memory celery. Celery is effectively running.
         return {}
 