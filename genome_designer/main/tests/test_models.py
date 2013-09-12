cefe661457168484959ede6dfce45a9829bd92fe
glebk <gleb.kuznetsov@gmail.com>
2013-09-12 13:11:19 -0400

    No need to use fake MEDIA_ROOT in test_models.py.
    We introduced a custom test suite runner in
    2485b46ff881a98b022f0c6bcb32468e3561f7b0, which handles creation and
    destruction of a separate test filesystem. Overriding
    settings.MEDIA_ROOT in test_models.py was interacting with this in a
    strange way, causing a `fake` dir to be created sometimes. Easiest fix
    is to get rid of the override.

--- genome_designer/main/tests/test_models.py
+++ genome_designer/main/tests/test_models.py
@@ -14,7 +14,6 @@ from scripts.import_util import import_reference_genome_from_local_file
 
 import settings
 
-FAKE_MEDIA_ROOT = 'fake/media/root'
 TEST_PROJECT_NAME = 'testModels_project'
 TEST_REF_GENOME_NAME = 'mg1655_partial'
 TEST_REF_GENOME_PATH =  os.path.join(settings.PWD,
@@ -31,9 +30,6 @@ class TestModels(TestCase):
         TEST_PASSWORD = 'password'
         TEST_EMAIL = 'test@example.com'
 
-
-        self.original_media_root = settings.MEDIA_ROOT
-        settings.MEDIA_ROOT = FAKE_MEDIA_ROOT
         user = User.objects.create_user(TEST_USERNAME, password=TEST_PASSWORD,
                 email=TEST_EMAIL)
 
@@ -47,15 +43,10 @@ class TestModels(TestCase):
             TEST_REF_GENOME_PATH,
             'genbank')
 
-    def tearDown(self):
-        """Override.
-        """
-        settings.MEDIA_ROOT = self.original_media_root
-
     def test_clean_filesystem_location(self):
         FAKE_ABS_ROOT = '/root/of/all/evil'
         EXPECTED_CLEAN_URL = 'projects/blah'
-        dirty_full_url = os.path.join(FAKE_ABS_ROOT, FAKE_MEDIA_ROOT,
+        dirty_full_url = os.path.join(FAKE_ABS_ROOT, settings.MEDIA_ROOT,
                 EXPECTED_CLEAN_URL)
         self.assertEqual(EXPECTED_CLEAN_URL,
                 clean_filesystem_location(dirty_full_url))
