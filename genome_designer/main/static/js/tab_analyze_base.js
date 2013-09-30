6fb35fd14f61643529d525eb4bb037db7ff8a27e
glebk <gleb.kuznetsov@gmail.com>
2013-09-30 13:26:10 -0400

    Revert incomplete DBG changes to UI from 4f61214939c514e0bd1689698bf768d92c7410fc.

--- genome_designer/main/static/js/tab_analyze_base.js
+++ genome_designer/main/static/js/tab_analyze_base.js
@@ -9,8 +9,6 @@ gd.TabAnalyzeBaseView = Backbone.View.extend({
 
   /** Backbone initialize method. */
   initialize: function() {
-    alert("intitialize is being run now!")
-
     // The DataTableComponent currently in view.
     this.datatable = null;
 
