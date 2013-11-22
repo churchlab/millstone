churchlab/millstone

commit d922023e8a1d4e7757bfa3e68aba3ace89f2a749

Added feature: Download reference genome from NCBI

genome_designer/main/views.py
@@ -33,6 +33,7 @@ from main.models import VariantSet
 from main.models import VariantToVariantSet
 from scripts.alignment_pipeline import create_alignment_groups_and_start_alignments
 from scripts.import_util import import_reference_genome_from_local_file
+from scripts.import_util import import_reference_genome_from_ncbi
 from scripts.import_util import import_samples_from_targets_file
 from scripts.import_util import import_variant_set_from_vcf
 from scripts.snp_callers import run_snp_calling_pipeline
@@ -191,13 +192,23 @@ def reference_genome_list_view(request, project_uid):
     if request.method == 'POST':
         # TODO: Add more inforative error handling.
         try:
-            import_reference_genome_from_local_file(
-                    project,
-                    request.POST['refGenomeLabel'],
-                    request.POST['refGenomeFileLocation'],
-                    request.POST['importFileFormat'])
+            if request.POST['postType'] == 'modalFromFile':
+                import_reference_genome_from_local_file(
+                        project,
+                        request.POST['refGenomeLabel'],
+                        request.POST['refGenomeFileLocation'],
+                        request.POST['importFileFormat'])
+            if request.POST['postType'] == 'modalFromNCBI':
+                import_reference_genome_from_ncbi(
+                        project,
+                        request.POST['refGenomeLabel'],
+                        request.POST['refGenomeAccession'],
+                        request.POST['importFileFormat'])
+            else:
+                raise InputError('POST has invalid or missing postType')
         except Exception as e:
             error_string = 'Import error: ' + str(e)
+            raise e
 
     context = {
         'project': project,

genome_designer/conf/global_settings.py
@@ -4,6 +4,9 @@ import os
 
 from django.conf import global_settings
 
+# EntrezGene wants an email to use it's API.
+EMAIL = "millstone_user@gmail.com"
+
 # The absolute path of the settings.py file's directory.
 # Useful for settings that require absolute paths like templates.
 PWD = os.path.join(os.path.dirname(os.path.realpath(__file__ )), '../')

genome_designer/main/templates/reference_genome_list.html
@@ -22,6 +22,7 @@
     <ul class="dropdown-menu">
       <li role="presentation"><a role="menuitem" tabindex="-1" href="#">From Variant Set...</a></li>
       <li role="presentation"><a role="menuitem" tabindex="-1" href="#modalFromFile" data-toggle="modal">From Server Location...</a></li>
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#modalFromNCBI" data-toggle="modal">From NCBI Accession...</a></li>
     </ul>
   </div>
 </p>
@@ -29,7 +30,7 @@
 <div id="gd-ref-genome-list-view-datatable-hook" class="container-fluid">
 </div>
 
-<!-- Modal -->
+<!-- ModalFromFile -->
 <div id="modalFromFile" class="modal hide fade" tabindex="-1" role="dialog">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal">×</button>
@@ -47,7 +48,36 @@
       <label class="radio">
         <input type="radio" name="importFileFormat" id="optionsRadios2" value="fasta">
         FASTA File <i>(No Gene/Effect Annotations)</i>
+      </label>2
+      <input type="hidden" name="postType" value="modalFromFile">
+    </div>
+    <div class="modal-footer">
+      <button class="btn" data-dismiss="modal">Cancel</button>
+      <button class="btn btn-primary" id="formFromFile">Submit</button>
+    </form>
+  </div>
+</div>
+
+<!-- ModalFromNCBI -->
+<div id="modalFromNCBI" class="modal hide fade" tabindex="-1" role="dialog">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal">×</button>
+    <h3 id="modalFromNCBI">Load File from NCBI</h3>
+  </div>
+  <div class="modal-body">
+    <form id="formFromFile" method="POST">
+      {% csrf_token %}
+      <input type="text" name="refGenomeLabel" placeholder="Reference Genome Name">
+      <input type="text" name="refGenomeAccession" placeholder="NCBI Nucleotide Accession">
+      <label class="radio">
+        <input type="radio" name="importFileFormat" id="optionsRadios3" value="genbank" checked>
+        Genbank File
       </label>
+      <label class="radio">
+        <input type="radio" name="importFileFormat" id="optionsRadios4" value="fasta">
+        FASTA File <i>(No Gene/Effect Annotations)</i>
+      </label>
+      <input type="hidden" name="postType" value="modalFromNCBI">
     </div>
     <div class="modal-footer">
       <button class="btn" data-dismiss="modal">Cancel</button>

genome_designer/scripts/import_util.py
@@ -9,6 +9,7 @@ import shutil
 import re
 import vcf
 from collections import namedtuple
+from tempfile import NamedTemporaryFile
 
 from django.db import transaction
 from main.models import clean_filesystem_location
@@ -23,8 +24,10 @@ from main.models import VariantToVariantSet
 from scripts.vcf_parser import extract_raw_data_dict
 from scripts.vcf_parser import get_or_create_variant
 from settings import PWD
+from settings import EMAIL
 
 from Bio import SeqIO
+from Bio import Entrez
 
 
 IMPORT_FORMAT_TO_DATASET_TYPE = {
@@ -33,24 +36,8 @@ IMPORT_FORMAT_TO_DATASET_TYPE = {
     'vcfu': Dataset.TYPE.VCF_USERINPUT
 }
 
-class DataImportError(Exception):
-    """Exception thrown when there are errors in imported data.
-
-    Attributes:
-        expr -- input expression in which the error occurred
-        msg  -- explanation of the error
-    """
-
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return 'DataImportError: ' + str(self.msg)
-
-
-
 def import_reference_genome_from_local_file(project, label, file_location,
-        import_format):
+        import_format, move=False):
     """Creates a ReferenceGenome associated with the given Project.
 
     Args:
@@ -58,6 +45,9 @@ def import_reference_genome_from_local_file(project, label, file_location,
         label: The human-readable label for the ReferenceGenome.
         file_location: Location of the genome on the server.
         import_format: Must be 'fasta' or 'genbank'.
+        move: move instead of copy the original file_location - for instance,
+        if we saved it to a temporary file. Moving is of course faster than
+        copying. 
     """
     # Validate the input.
     assert import_format in ['fasta', 'genbank']
@@ -73,10 +63,6 @@ def import_reference_genome_from_local_file(project, label, file_location,
     for genome_record in SeqIO.parse(file_location, import_format):
         num_chromosomes += 1
         num_bases += len(genome_record)
-
-    # Make sure sequence exists.
-    if not num_bases > 0:
-        raise DataImportError("No sequence in file.")
 
     # Create the ReferenceGenome object.
     reference_genome = ReferenceGenome.objects.create(
@@ -94,8 +80,7 @@ def import_reference_genome_from_local_file(project, label, file_location,
 
 def generate_fasta_from_genbank(ref_genome):
     """If this reference genome has a genbank but not a FASTA, generate
-    a FASTA from the genbank.
-    """
+    a FASTA from the genbank. """
 
     # If a FASTA already exists, then just return.
     if ref_genome.dataset_set.filter(
@@ -127,12 +112,37 @@ def generate_fasta_from_genbank(ref_genome):
 
     return
 
-def import_ref_genome_from_genbank():
+def import_reference_genome_from_ncbi(project, label, record_id, import_format):
     """
-    Pull a reference genome by accession from NCBI using efetch. 
+    Pull a reference genome by accession from NCBI using efetch.
     """
-    raise NotImplementedError
+    # Validate the input.
+    assert import_format in ['fasta', 'genbank']
 
+    # Format keys for Efetch.
+    # More info here: 
+    # http://www.ncbi.nlm.nih.gov/
+    #       books/NBK25499/table/chapter4.chapter4_table1/?report=objectonly
+    CONVERT_FORMAT = {'fasta':'fa', 'genbank':'gbwithparts'}
+
+    Entrez.email = EMAIL
+    handle = Entrez.efetch(
+            db="nuccore", 
+            id=record_id, 
+            rettype=CONVERT_FORMAT[import_format], 
+            retmode="text")
+
+    temp = NamedTemporaryFile(delete=False)
+    temp.write(handle.read())
+    handle.close()
+    temp.close()
+    reference_genome = import_reference_genome_from_local_file(
+        project, label, temp.name, import_format, move=True)
+
+    if os.path.isfile(temp.name):
+        os.remove(temp.name)
+
+    return reference_genome
 
 def sanitize_record_id(record_id_string):
     """We want to grab only the first word-only part of each seqrecord in a
@@ -256,7 +266,6 @@ def import_variant_set_from_vcf(ref_genome, variant_set_name, variant_set_file):
     _read_variant_set_file_as_csv(variant_set_file, ref_genome, dataset,
             variant_set)
 
-
 def _read_variant_set_file_as_csv(variant_set_file, reference_genome,
         dataset, variant_set):
     """If reading the variant set file as a vcf fails (because we arent using
@@ -315,25 +324,32 @@ def _read_variant_set_file_as_csv(variant_set_file, reference_genome,
 
 
 def copy_and_add_dataset_source(entity, dataset_label, dataset_type,
-        original_source_location):
+        original_source_location, move=False):
     """Copies the dataset to the entity location and then adds as
-    Dataset.
+    Dataset. If the original_source_location is a file object, then
+    it just read()s from the handle and writes to destination. 
 
     The model entity must satisfy the following interface:
         * property dataset_set
         * method get_model_data_dir()
 
     Returns the Dataset object.
+
+    If move is true, move instead of copying it. Good for files downloaded
+    to a temp directory, since copying is slower.
     """
-    dest = copy_dataset_to_entity_data_dir(entity, original_source_location)
+    dest = copy_dataset_to_entity_data_dir(entity, original_source_location,
+        move)
     dataset = add_dataset_to_entity(entity, dataset_label, dataset_type,
         dest)
 
     return dataset
 
 
-def copy_dataset_to_entity_data_dir(entity, original_source_location):
-    """Copies the data to the entity model data dir.
+def copy_dataset_to_entity_data_dir(entity, original_source_location,
+        move=False):
+    """If a file path, copy the data to the entity model data dir.
+       If a handle, then just write it to the data dir.
 
     Returns:
         The destination to which the file was copied.
@@ -341,8 +357,17 @@ def copy_dataset_to_entity_data_dir(entity, original_source_location):
     assert hasattr(entity, 'get_model_data_dir')
     source_name = os.path.split(original_source_location)[1]
     dest = os.path.join(entity.get_model_data_dir(), source_name)
+
+
     if not original_source_location == dest:
-        shutil.copy(original_source_location, dest)
+        try: #first try path
+            if move:
+                shutil.move(original_source_location, dest)
+            else:
+                shutil.copy(original_source_location, dest)
+        except TypeError: #then try a handle
+            open(dest,'w').write(
+                original_source_location.read())
     return dest
 
 

genome_designer/scripts/tests/test_import_util.py
@@ -15,50 +15,34 @@ from main.models import ReferenceGenome
 from main.models import Variant
 from main.models import VariantSet
 from scripts.dynamic_snp_filter_key_map import initialize_filter_key_map
-from scripts.import_util import DataImportError
-from scripts.import_util import import_reference_genome_from_local_file
 from scripts.import_util import import_samples_from_targets_file
 from scripts.import_util import import_variant_set_from_vcf
+from scripts.import_util import import_reference_genome_from_ncbi
 from settings import PWD as GD_ROOT_PATH
-
+from scripts.util import internet_on
 
 TEST_USERNAME = 'gmcdev'
 TEST_PASSWORD = 'g3n3d3z'
 TEST_EMAIL = 'gmcdev@genomedesigner.freelogy.org'
 
-
-class TestImportReferenceGenome(TestCase):
-    """Tests importing a ReferenceGenome.
+class TestImportRefGenomeFromEntrez(TestCase):
+    """Test grabbing a genome from teh interweb. Requires internet access.
     """
 
     def setUp(self):
         # Test models.
         user = User.objects.create_user(TEST_USERNAME, password=TEST_PASSWORD,
                 email=TEST_EMAIL)
-        self.project = Project.objects.create(owner=user.get_profile(),
+        self.test_project = Project.objects.create(owner=user.get_profile(),
                 title='Test Project')
 
-
-    def test_import_reference_genome_from_local_file(self):
-        """Tests importing reference genome.
-        """
-        TEST_GENBANK_FILE = os.path.join(GD_ROOT_PATH,
-                'test_data', 'import_util_test_data', 'mini_mg1655.genbank')
-
-        import_reference_genome_from_local_file(self.project, 'a label',
-                TEST_GENBANK_FILE, 'genbank')
-
-
-    def test_import_reference_genome_from_local_file__fail_if_no_seq(self):
-        """Should fail if no sequence in file.
-        """
-        TEST_GENBANK_FILE__NO_SEQ = os.path.join(GD_ROOT_PATH,
-                'test_data', 'import_util_test_data', 'mg1655_no_seq.genbank')
-
-        with self.assertRaises(DataImportError):
-            import_reference_genome_from_local_file(self.project, 'a label',
-                    TEST_GENBANK_FILE__NO_SEQ, 'genbank')
-
+    def test_import_entrez(self):
+        TEST_RECORD_ID = "6273291"
+        TEST_RECORD_LABEL = "testRecord"
+        if internet_on():
+            import_reference_genome_from_ncbi(
+                    self.test_project, TEST_RECORD_LABEL, TEST_RECORD_ID, 
+                    'genbank')
 
 class TestImportSamplesFromTargetsFile(TestCase):
     """Tests for scripts.import_util.import_samples_from_targets_file().

genome_designer/scripts/util.py
@@ -48,3 +48,12 @@ def fn_runner(fn, args_list, concurrent=False):
         return generic_task.delay(fn.__name__, args_list)
     else:
         fn(*args_list)
+
+import urllib2
+
+def internet_on():
+    try:
+        response=urllib2.urlopen('http://74.125.228.100',timeout=1)
+        return True
+    except urllib2.URLError as err: pass
+    return False

genome_designer/test_data/full_vcf_test_set/mg1655_tolC_through_zupT.gb
@@ -1,7 +1,7 @@
-LOCUS       NC_000913               5652 bp    DNA     linear   UNA 06-SEP-2013
+LOCUS       NC_000913.1             5652 bp    DNA     linear   UNA 06-SEP-2013
 DEFINITION  Escherichia coli str. K-12 substr. MG1655, complete genome.
-ACCESSION   NC_000913
-VERSION     NC_000913
+ACCESSION   NC_000913.1
+VERSION     NC_000913.1
 KEYWORDS    .
 SOURCE      Escherichia coli str. K-12 substr. MG1655
   ORGANISM  Escherichia coli str. K-12 substr. MG1655

commit d7aed1906636274a3ac489ddc16efc165ff50d0a

Fix bug when importing ref genome. s/if/elif.
Bug introduced in d922023e8a1d4e7757bfa3e68aba3ace89f2a749.

genome_designer/main/views.py
@@ -44,6 +44,19 @@ TAB_ROOT__DATA = 'DATA'
 TAB_ROOT__ALIGN = 'ALIGN'
 TAB_ROOT__ANALYZE = 'ANALYZE'
 
+
+class InputError(Exception):
+    """Exception raised for errors in the input.
+
+    I assume this is what DBG wanted in d922023e8a1d4e7757bfa3e68aba3ace89f2a749.
+
+    Attributes:
+        msg  -- explanation of the error
+    """
+    def __init__(self, msg):
+        self.msg = msg
+
+
 def home_view(request):
     """The main landing page.
     """
@@ -198,7 +211,7 @@ def reference_genome_list_view(request, project_uid):
                         request.POST['refGenomeLabel'],
                         request.POST['refGenomeFileLocation'],
                         request.POST['importFileFormat'])
-            if request.POST['postType'] == 'modalFromNCBI':
+            elif request.POST['postType'] == 'modalFromNCBI':
                 import_reference_genome_from_ncbi(
                         project,
                         request.POST['refGenomeLabel'],
@@ -208,7 +221,6 @@ def reference_genome_list_view(request, project_uid):
                 raise InputError('POST has invalid or missing postType')
         except Exception as e:
             error_string = 'Import error: ' + str(e)
-            raise e
 
     context = {
         'project': project,