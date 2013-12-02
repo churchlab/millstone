3a67e41186c77a824f04d72535c51b03d207d270
Changping Chen <ccp0101@gmail.com>
2013-12-02 18:26:40 -0500

    worker setup interface

---
+++ celery_manager/celery_manager/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+import os.path
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'celery_manager.views.home', name='home'),
+    url(r'^test$', 'celery_manager.views.test', name='test'),
+    url(r'^save$', 'celery_manager.views.save', name='save'),
+) + static(settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, "static"))
---
+++ celery_manager/celery_manager/views.py
@@ -0,0 +1,102 @@
+from django.http import Http404
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from django.conf import settings
+import psycopg2
+import json
+import os
+import kombu
+
+
+def get_local_settings_path():
+    path = os.path.join(settings.BASE_DIR, "../../genome_designer/conf/local_settings.py")
+    assert os.path.isfile(path)
+    return path
+
+def home(request):
+    path = get_local_settings_path()
+    with open(path) as f:
+        code = compile(f.read(), path, 'exec')
+    config = {}
+    exec code in config
+
+    c = {
+        'dbname': config.get('DATABASES', {}).get('default', {}).get('NAME', 'genome-designer'),
+        'user': config.get('DATABASES', {}).get('default', {}).get('USER', 'genome-designer'),
+        'password': config.get('DATABASES', {}).get('default', {}).get('PASSWORD', ''),
+        'host': config.get('DATABASES', {}).get('default', {}).get('HOST', ''),
+    }
+    return render(request, 'base.html', c)
+
+@require_POST
+def test(request):
+    user = request.POST['user']
+    password = request.POST['password']
+    host = request.POST['host']
+    dbname = request.POST['dbname']
+
+    try:
+        connstring = "dbname='%s' user='%s' host='%s' password='%s' port=5432 connect_timeout=5" % (
+            dbname, user, host, password)
+        conn = psycopg2.connect(connstring)
+    except Exception as e:
+        print e
+        response = {
+            'status': 'error',
+            'message': "Cannot connect to PostgresSQL: " + str(e),
+            'connstring': connstring
+        }
+        return HttpResponse(json.dumps(response),
+            content_type='application/json')
+
+    try:
+        connstring = 'amqp://%s:%s@%s:5672/' % (user, password, host)
+        conn = kombu.Connection()
+        conn.connect()
+    except Exception as e:
+        print e
+        response = {
+            'status': 'error',
+            'message': "Cannot connect to RabbitMQ: " + str(e),
+            'connstring': connstring
+        }
+        return HttpResponse(json.dumps(response),
+            content_type='application/json')
+
+    response = {
+        'status': 'success',
+    }
+    return HttpResponse(json.dumps(response),
+        content_type='application/json')
+
+def save(request):
+    user = request.POST['user']
+    password = request.POST['password']
+    host = request.POST['host']
+    dbname = request.POST['dbname']
+
+    db = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': dbname,
+            'USER': user,
+            'PASSWORD': password,
+            'HOST': host,
+            'PORT': 5432,
+        }
+    }
+    config = """DATABASES = %s """ % repr(db)
+    path = get_local_settings_path()
+
+    with open(path, "w") as f:
+        f.write(config)
+
+    response = {
+        'status': 'success'
+    }
+    return HttpResponse(json.dumps(response),
+        content_type='application/json')
