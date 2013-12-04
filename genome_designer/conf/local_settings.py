6f6e8c89fa6a98111a49cb77b99e0f5c6775da51
Changping Chen <ccp0101@gmail.com>
2013-12-04 17:06:46 -0500

    revert local settings

--- genome_designer/conf/local_settings.py
+++ genome_designer/conf/local_settings.py
@@ -1,2 +1,2 @@
-DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': u'x', 'HOST': u'127.0.0.1', 'USER': u'ccp', 'PASSWORD': u'x', 'PORT': 5432}} 
-BROKER_URL = u'amqp://genome_designer:x@127.0.0.1:5672' 
+# Add settings overrides here.
+# Changes to this file are ignored by git.
