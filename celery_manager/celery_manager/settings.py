1d930b3f16ef3782092084384906397d57813f55
Changping Chen <ccp0101@gmail.com>
2013-12-04 15:33:10 -0500

    relative static url

--- celery_manager/celery_manager/settings.py
+++ celery_manager/celery_manager/settings.py
@@ -69,7 +69,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
