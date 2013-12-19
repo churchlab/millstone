31df5dcbd14094aababc13d7c471d5b8f9ec1c26
glebk <gleb.kuznetsov@gmail.com>
2013-12-19 17:52:33 -0500

    Better code for building materialized variant table.

--- genome_designer/variants/materialized_view_manager.py
+++ genome_designer/variants/materialized_view_manager.py
@@ -52,13 +52,42 @@ class AbstractMaterializedViewManager(object):
         transaction.commit_unless_managed()
 
 
-SELECT_FIELDS = [
-    'main_variant.id',
-    'main_variant.position',
-    'main_variant.chromosome',
-    'main_experimentsample.id AS experiment_sample_id',
-    'main_experimentsample.uid AS experiment_sample_uid'
+# Build the schema used to build the materialized view.
+# All of this happens on the first module import.
+class SchemaBuilder(object):
+    def __init__(self):
+        self.schema = []
+
+        # Check for duplicates while building.
+        self.joined_table_col_name_set = set()
+
+    def add_melted_variant_field(self, source_col_name, joined_table_col_name,
+            is_user_queryable):
+        assert joined_table_col_name not in self.joined_table_col_name_set
+        self.schema.append({
+            'source_col_name': source_col_name,
+            'joined_table_col_name': joined_table_col_name,
+            'is_user_queryable': is_user_queryable,
+        })
+        self.joined_table_col_name_set.add(joined_table_col_name)
+
+    def get_schema(self):
+        return self.schema
+
+SCHEMA_BUILDER = SchemaBuilder()
+SCHEMA_BUILDER.add_melted_variant_field('main_variant.id', 'id', False)
+SCHEMA_BUILDER.add_melted_variant_field('main_variant.position', 'position', True)
+SCHEMA_BUILDER.add_melted_variant_field('main_variant.chromosome', 'chromosome', True)
+SCHEMA_BUILDER.add_melted_variant_field('main_experimentsample.id', 'experiment_sample_id', False)
+SCHEMA_BUILDER.add_melted_variant_field('main_experimentsample.uid', 'experiment_sample_uid', True)
+MELTED_VARIANT_SCHEMA = SCHEMA_BUILDER.get_schema()
+
+# Generate the SELECT clause.
+MATERIALIZED_TABLE_SELECT_CLAUSE_COMPONENTS = [
+        schema_obj['source_col_name'] + ' AS ' + schema_obj['joined_table_col_name']
+        for schema_obj in MELTED_VARIANT_SCHEMA
 ]
+MATERIALIZED_TABLE_SELECT_CLAUSE = ', '.join(MATERIALIZED_TABLE_SELECT_CLAUSE_COMPONENTS)
 
 class MeltedVariantMaterializedViewManager(AbstractMaterializedViewManager):
     """Interface for objects providing a wrapper for a Postgresql materialized
@@ -81,7 +110,7 @@ class MeltedVariantMaterializedViewManager(AbstractMaterializedViewManager):
                     'INNER JOIN main_experimentsample ON (main_variantevidence.experiment_sample_id = main_experimentsample.id) '
                 'WHERE (main_variant.reference_genome_id = %d) '
                 'ORDER BY main_variant.position'
-            % (self.view_table_name, ', '.join(SELECT_FIELDS), self.reference_genome.id)
+            % (self.view_table_name, MATERIALIZED_TABLE_SELECT_CLAUSE, self.reference_genome.id)
         )
         self.cursor.execute(create_sql_statement)
         transaction.commit_unless_managed()
