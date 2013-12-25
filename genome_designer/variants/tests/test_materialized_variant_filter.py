1e52ae7833552b764bb7938c65a31d4d3764f9eb
glebk <gleb.kuznetsov@gmail.com>
2013-12-25 13:07:21 -0700

    Copy over more variant filtering tests (simple ones) to be tested by new materialized view implementation.

--- genome_designer/variants/tests/test_materialized_variant_filter.py
+++ genome_designer/variants/tests/test_materialized_variant_filter.py
@@ -23,6 +23,7 @@ from main.models import VariantToVariantSet
 from scripts.dynamic_snp_filter_key_map import initialize_filter_key_map
 from scripts.dynamic_snp_filter_key_map import update_filter_key_map
 from settings import PWD as GD_ROOT
+from variants.common import ParseError
 from variants.materialized_variant_filter import get_variants_that_pass_filter
 from variants.materialized_view_manager import MeltedVariantMaterializedViewManager
 
@@ -69,21 +70,23 @@ class BaseTestVariantFilterTestCase(TestCase):
                 num_reads=100,
         )
 
+        # Only variants that are part of a set will be returned in the melted
+        # view. If a Variant is not associatd with a VariantSet, nor is it
+        # associated with Sample, then the materialized view will not return
+        # it.
+        self.catchall_variant_set = VariantSet.objects.create(
+                reference_genome=self.ref_genome,
+                label='catchall')
+
         self.materialized_view_manager = MeltedVariantMaterializedViewManager(
                 self.ref_genome)
 
 
 class TestVariantFilter(BaseTestVariantFilterTestCase):
+
     def test_filter__by_position(self):
         """Test filtering by position.
         """
-        # Only variants that are part of a set will be returned in the melted
-        # view. If a Variant is not associatd with a VariantSet, nor is it
-        # associated with Sample, then the materialized view will not return
-        # it.
-        var_set = VariantSet.objects.create(
-                reference_genome=self.ref_genome,
-                label='set1')
 
         # Create several Variants with positions:
         # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
@@ -100,7 +103,8 @@ class TestVariantFilter(BaseTestVariantFilterTestCase):
                             variant=var,
                             alt_value='G'))
 
-            VariantToVariantSet.objects.create(variant=var, variant_set=var_set)
+            VariantToVariantSet.objects.create(variant=var,
+                    variant_set=self.catchall_variant_set)
 
         # Test querying Variants with position > 5.
         result = get_variants_that_pass_filter('position > 5', self.ref_genome)
@@ -115,6 +119,133 @@ class TestVariantFilter(BaseTestVariantFilterTestCase):
         self.assertEqual(5, len(variants_above_5))
         for var in variants_above_5:
             self.assertTrue(var['position'] >= 5)
+
+
+    def test_filter__by_chromosome(self):
+        """Test filtering by chromosome.
+        """
+        CHROM_1 = 'chrom'
+        for pos in range(6):
+            var = Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_1,
+                position=pos,
+                ref_value='A')
+
+            var.variantalternate_set.add(
+                    VariantAlternate.objects.create(
+                            variant=var,
+                            alt_value='G'))
+
+            VariantToVariantSet.objects.create(variant=var,
+                    variant_set=self.catchall_variant_set)
+
+        CHROM_2 = 'chrom2'
+        for pos in range(9):
+            var = Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_2,
+                position=pos,
+                ref_value='A')
+
+            var.variantalternate_set.add(
+                    VariantAlternate.objects.create(
+                            variant=var,
+                            alt_value='G'))
+
+            VariantToVariantSet.objects.create(variant=var,
+                    variant_set=self.catchall_variant_set)
+
+        def _assert_results(chromosome_value, num_results):
+            result = get_variants_that_pass_filter(
+                    'chromosome = %s'% chromosome_value, self.ref_genome)
+            variants = result.variant_set
+            self.assertEqual(num_results, len(variants))
+            for var in variants:
+                self.assertEqual(chromosome_value, var['chromosome'])
+        _assert_results(CHROM_1, 6)
+        _assert_results(CHROM_2, 9)
+
+
+    def test_filter__by_position_and_chromosome(self):
+        """Test filtering by position.
+        """
+        CHROM_1 = 'chrom'
+        for pos in range(6):
+            var = Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_1,
+                position=pos,
+                ref_value='A')
+
+            var.variantalternate_set.add(
+                    VariantAlternate.objects.create(
+                            variant=var,
+                            alt_value='G'))
+
+            VariantToVariantSet.objects.create(variant=var,
+                    variant_set=self.catchall_variant_set)
+
+        CHROM_2 = 'chrom2'
+        for pos in range(9):
+            var = Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_2,
+                position=pos,
+                ref_value='A')
+
+            var.variantalternate_set.add(
+                    VariantAlternate.objects.create(
+                            variant=var,
+                            alt_value='G'))
+
+            VariantToVariantSet.objects.create(variant=var,
+                    variant_set=self.catchall_variant_set)
+
+
+        def _assert_results(query, num_results, chromosome_value,
+                position_value, position_delim):
+            result = get_variants_that_pass_filter(query, self.ref_genome)
+            variants = result.variant_set
+            self.assertEqual(num_results, len(variants))
+            for var in variants:
+                self.assertEqual(chromosome_value, var['chromosome'])
+                self.assertTrue(eval(
+                        str(var['position']) + position_delim +
+                                str(position_value)))
+
+        QUERY_STRING = 'position > 4 & chromosome = chrom'
+        _assert_results(QUERY_STRING, 1, CHROM_1, 4, '>')
+
+        QUERY_STRING = 'position >= 5 & chromosome = chrom2'
+        _assert_results(QUERY_STRING, 4, CHROM_2, 5, '>=')
+
+
+    def test_filter__invalid_key(self):
+        CHROM_1 = 'chrom'
+        for pos in range(6):
+            var = Variant.objects.create(
+                type=Variant.TYPE.TRANSITION,
+                reference_genome=self.ref_genome,
+                chromosome=CHROM_1,
+                position=pos,
+                ref_value='A')
+
+            var.variantalternate_set.add(
+                    VariantAlternate.objects.create(
+                            variant=var,
+                            alt_value='G'))
+
+            VariantToVariantSet.objects.create(variant=var,
+                    variant_set=self.catchall_variant_set)
+
+        QUERY_STRING = 'dinosaur > 4 & chromosome = chrom'
+        with self.assertRaises(ParseError):
+            get_variants_that_pass_filter(QUERY_STRING, self.ref_genome)
 
 
 class TestMinimal(BaseTestVariantFilterTestCase):
