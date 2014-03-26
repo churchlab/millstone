8d2673dc9cd8dfae82cb393855f16d0663cf6e3e
glebk <gleb.kuznetsov@gmail.com>
2014-03-26 19:48:53 -0400

    Set default ALLOWED_HOSTS to allow DEBUG = False to work.

--- genome_designer/conf/global_settings.py
+++ genome_designer/conf/global_settings.py
@@ -58,7 +58,10 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
