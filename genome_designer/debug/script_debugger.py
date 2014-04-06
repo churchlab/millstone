8ec9d1fb9055e5714437930358bcc6da94e2d081
glebk <gleb.kuznetsov@gmail.com>
2014-04-06 16:50:54 -0400

    Organize script_debugger.py to allow better organization of debug sessions.

--- genome_designer/debug/script_debugger.py
+++ genome_designer/debug/script_debugger.py
@@ -8,13 +8,24 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
+from main.celery_util import assert_celery_running
 from main.models import *
 from variants import materialized_view_manager
 
-def main():
+
+def debug_materialized_view_manager():
     ref = ReferenceGenome.objects.get(alignmentgroup__uid='c359d2cf')
     mvm = materialized_view_manager.MeltedVariantMaterializedViewManager(ref)
     mvm.create()
+
+
+def debug_celery():
+    assert_celery_running()
+
+
+def main():
+    debug_celery()
+
 
 if __name__ == '__main__':
     main()
