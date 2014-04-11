f87df24214d17d0d3092fedd9514f046099f2899
glebk <gleb.kuznetsov@gmail.com>
2014-04-11 18:43:13 -0400

    Fix broken upload-samples-through-browser template.
    Add test to prevent it from happening again.

---
+++ genome_designer/main/tests/test_upload_template_views.py
@@ -0,0 +1,47 @@
+import csv
+import StringIO
+
+from django.core.urlresolvers import reverse
+from django.test import Client
+from django.test import TestCase
+
+from main.upload_template_views import SAMPLE_LIST_BROWSER_UPLOAD_TEMPLATE
+from scripts.import_util import SAMPLE_BROWSER_UPLOAD_KEY__SAMPLE_NAME
+from scripts.import_util import SAMPLE_BROWSER_UPLOAD_KEY__READ_1
+from scripts.import_util import SAMPLE_BROWSER_UPLOAD_KEY__READ_2
+
+
+class TestSampleListBrowserUploadTemplate(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_template(self):
+        """Makes sure the template is as expected.
+
+        The main issue we're testing here is that we don't accidentally
+        break our template.
+        """
+        # Make the request.
+        template_url  = reverse(
+                'main.upload_template_views.sample_list_browser_upload_template')
+        response = self.client.get(template_url)
+
+        # Create a reader on the response content.
+        reader = csv.DictReader(StringIO.StringIO(response.content),
+                delimiter='\t')
+
+        # Check that the expected headrs are there.
+        targets_file_header = reader.fieldnames
+        EXPECTED_TEMPLATE_HEADER = [
+                SAMPLE_BROWSER_UPLOAD_KEY__SAMPLE_NAME,
+                SAMPLE_BROWSER_UPLOAD_KEY__READ_1,
+                SAMPLE_BROWSER_UPLOAD_KEY__READ_2
+        ]
+
+        missing_header_cols = (set(EXPECTED_TEMPLATE_HEADER) -
+            set(targets_file_header))
+        self.assertEqual(0, len(missing_header_cols),
+            "Missing cols: %s. "
+            "Make sure the template is properly tab-delimited." %
+            ' '.join(missing_header_cols))
--- genome_designer/main/upload_template_views.py
+++ genome_designer/main/upload_template_views.py
@@ -2,11 +2,12 @@
 Handlers for fetching upload templates.
 """
 
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-@login_required
+SAMPLE_LIST_BROWSER_UPLOAD_TEMPLATE = 'sample_list_browser_upload_template.tsv'
+
+
 def sample_list_targets_template(request):
     """Let the user download a blank sample targets template as a tab
     separated values file (.tsv) so they can fill it in and upload
@@ -17,7 +18,6 @@ def sample_list_targets_template(request):
             content_type='text/tab-separated-values')
 
 
-@login_required
 def variant_set_upload_template(request):
     """Let the user download a blank variant set template as a blank
     VCF file to be filled in.
@@ -27,11 +27,10 @@ def variant_set_upload_template(request):
             content_type='text/tab-separated-values')
 
 
-@login_required
 def sample_list_browser_upload_template(request):
     """Template that allows the user to indicate the names of samples they
     will updload through the browser form.
     """
     context = {}
-    return render(request, 'sample_list_browser_upload_template.tsv', context,
+    return render(request, SAMPLE_LIST_BROWSER_UPLOAD_TEMPLATE, context,
             content_type='text/tab-separated-values')
