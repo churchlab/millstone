cfcf69885a50bee2627a36a934b2662f8c8b27ca
glebk <gleb.kuznetsov@gmail.com>
2014-04-12 13:23:05 -0400

    Fix #272: Global JavaScript 500 catcher that shows alert and requests user to file a bug.

---
+++ genome_designer/main/static/js/global_error_catcher.js
@@ -0,0 +1,14 @@
+/**
+ * Catches any 500 errors and shows an alert to the user with a link to report
+ * the bug. Otherwise, the error is hidden in the console and most users
+ * won't know what's going on.
+ */
+
+ $(document).ajaxError(function (e, xhr, options) {
+  var url = e.currentTarget.URL;
+  alert(
+      'Server Error. Please file an issue at ' +
+      'https://github.com/churchlab/millstone/issues/new. ' +
+      'Describe what you were trying to do, and include the following ' +
+      'offending URL: ' + url);
+ });
