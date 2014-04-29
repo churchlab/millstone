churchlab/millstone

commit 1f5b7c9312c35e215659ec16da7f4f14c6675d04

Add INFO column for extra information on each variant.
Shows AA info for SNVs, and type & length for SVs.

genome_designer/main/model_views.py
@@ -155,6 +155,24 @@ def adapt_non_recursive(obj_list, field_dict_list, reference_genome, melted):
             # field config object constructed separately below.
             visible_field_pairs.append((field, value))
 
+        # Append catch all INFO column field
+        # TODO use variables instead of magic strings
+        va_data = melted_variant_obj['VA_DATA']
+        vccd_data = melted_variant_obj['VCCD_DATA']
+        if vccd_data and vccd_data[0]:
+            # is SV: make info of the form "SV [type] [length]"
+            if 'INFO_SVTYPE' in vccd_data[0]:
+                visible_field_pairs.append(('INFO', 'SV %s %d' %
+                    (vccd_data[0]['INFO_SVTYPE'], vccd_data[0].get('INFO_SVLEN', ''))))
+            # is SNP: make info equal to the AA field
+            elif va_data and va_data[0]:  # is SNP
+                visible_field_pairs.append(('INFO', va_data[0].get('INFO_EFF_AA', '')))
+            # unknown: just leave info field blank
+            else:
+                visible_field_pairs.append(('INFO', ''))
+        else:
+            visible_field_pairs.append(('INFO', ''))
+
         fe_obj_list.append(dict(visible_field_pairs))
 
     # Create the config dict that tells DataTables js how to display each col.
@@ -181,6 +199,14 @@ def adapt_non_recursive(obj_list, field_dict_list, reference_genome, melted):
         if fdict.get('last'):
             last_idxes.append(displayed_idx)
         displayed_idx += 1
+
+    # Add metadata for INFO column
+    obj_field_config.append({
+      'mData': 'INFO',
+      'sTitle': 'Info',
+      'bSortable': False
+      })
+    displayed_idx += 1
 
     # Finally, move these fields to the end.
     for i in last_idxes:
@@ -436,7 +462,6 @@ OPTIONAL_DEFAULT_FIELDS = [
     {'field': 'INFO_EFF_GENE', 'verbose': 'Gene', 'format': 'gather'}, # va_data
     {'field': 'INFO_EFF_IMPACT', 'verbose': 'Impact', 'format': 'gather',
             'recase':'title'}, # va_data
-    {'field': 'INFO_EFF_AA', 'verbose': 'AA', 'format': 'gather'}, # va_data
 ]
 
 

genome_designer/pipeline/snv_calling.py
@@ -377,8 +377,7 @@ def postprocess_pindel_vcf(vcf_file):
 
     # add some additional VCF fields
     with open(vcf_file + '.tmp', 'w') as fh:
-        fh.write('##INFO=<ID=SVMETHOD,Number=1,Type=String,Description="Type of approach used to detect SV">\n')
-        fh.write('##INFO=<ID=EFF_IMPACT,Number=A,Type=String,Description="Effect impact {High, Moderate, Low, Modifier}.">\n')
+        fh.write('##INFO=<ID=METHOD,Number=1,Type=String,Description="Type of approach used to detect SV">\n')
 
     vcf_writer = vcf.Writer(open(vcf_file + '.tmp', 'a'), vcf_reader)
     for record in vcf_reader:
@@ -391,9 +390,7 @@ def postprocess_pindel_vcf(vcf_file):
             continue
 
         # update some fields
-        record.__dict__['INFO']['SVMETHOD'] = 'PINDEL'
-        record.__dict__['INFO']['EFF_IMPACT'] = '%s_%d' % \
-                (record.__dict__['INFO'].get('SVTYPE', ''), svlen)
+        record.__dict__['INFO']['METHOD'] = 'PINDEL'
 
         vcf_writer.write_record(record)
 
@@ -405,15 +402,12 @@ def postprocess_delly_vcf(vcf_file):
 
     # add some additional VCF fields
     with open(vcf_file + '.tmp', 'w') as fh:
-        fh.write('##INFO=<ID=EFF_IMPACT,Number=A,Type=String,Description="Effect impact {High, Moderate, Low, Modifier}.">\n')
+        fh.write('##INFO=<ID=METHOD,Number=1,Type=String,Description="Type of approach used to detect SV">\n')
 
-    vcf_writer = vcf.Writer(open(vcf_file + '.tmp', 'w'), vcf_reader)
+    vcf_writer = vcf.Writer(open(vcf_file + '.tmp', 'a'), vcf_reader)
     for record in vcf_reader:
         # update some fields
-        record.__dict__['INFO']['SVMETHOD'] = 'DELLY'
-        record.__dict__['INFO']['EFF_IMPACT'] = '%s_%d' % \
-                (record.__dict__['INFO'].get('SVTYPE', ''),
-                record.__dict__['INFO'].get('SVLEN', 0))
+        record.__dict__['INFO']['METHOD'] = 'DELLY'
 
         vcf_writer.write_record(record)
 

genome_designer/variants/melted_variant_schema.py
@@ -174,7 +174,8 @@ MATERIALIZED_TABLE_QUERY_SELECT_CLAUSE_COMPONENTS = [
 ] + [
     'va_data', # Needed to hard-code showing INFO_EFF_GENE,
     'es_data',
-    've_data'
+    've_data',
+    'vccd_data'
 ]
 
 # Map from queryable fields to schema info (e.g. type, num).

commit 4ed72bd82d6e1774f42239cea90a1d90f1cbaa04

Fix #317: vccd_data for SVs is list, but for SNPs it's object.
This bug was introduced in 1f5b7c9312c35e215659ec16da7f4f14c6675d04.

genome_designer/main/model_views.py
@@ -162,7 +162,7 @@ def adapt_non_recursive(obj_list, field_dict_list, reference_genome, melted):
         # TODO use variables instead of magic strings
         va_data = melted_variant_obj['VA_DATA']
         vccd_data = melted_variant_obj['VCCD_DATA']
-        if vccd_data and vccd_data[0]:
+        if isinstance(vccd_data, list) and len(vccd_data) > 0 and vccd_data[0]:
             # is SV: make info of the form "SV [type] [length]"
             if 'INFO_SVTYPE' in vccd_data[0]:
                 visible_field_pairs.append(('INFO', 'SV %s %d' %

genome_designer/main/tests/test_xhr_handlers.py
@@ -170,6 +170,83 @@ class TestGetVariantList(TestCase):
                 for obj in variant_obj_list])
         self.assertEqual(set(range(TOTAL_NUM_VARIANTS)), variant_position_set)
 
+    def test_melted(self):
+        """Test melted view.
+        """
+        alignment_group = AlignmentGroup.objects.create(
+            label='Alignment 1',
+            reference_genome=self.ref_genome,
+            aligner=AlignmentGroup.ALIGNER.BWA)
+
+        TOTAL_NUM_VARIANTS = 10
+        for pos in range(TOTAL_NUM_VARIANTS):
+            # We need all these models for testing because this is what the
+            # materialized view create requires to return non-null results.
+            variant = Variant.objects.create(
+                    type=Variant.TYPE.TRANSITION,
+                    reference_genome=self.ref_genome,
+                    chromosome='chrom',
+                    position=pos,
+                    ref_value='A')
+
+            VariantAlternate.objects.create(
+                variant=variant,
+                alt_value='G')
+
+            common_data_obj = VariantCallerCommonData.objects.create(
+                variant=variant,
+                source_dataset=self.vcf_dataset,
+                alignment_group=alignment_group,
+                data={u'INFO_DP': 20, u'INFO_PQR': 0.0})
+
+            VariantEvidence.objects.create(
+                experiment_sample=self.sample_obj_1,
+                variant_caller_common_data=common_data_obj)
+
+        # Sanity check that the Variants were actually created.
+        self.assertEqual(TOTAL_NUM_VARIANTS, Variant.objects.filter(
+                reference_genome=self.ref_genome,).count())
+
+        request_data = {
+            'refGenomeUid': self.ref_genome.uid,
+            'projectUid': self.project.uid,
+            'melt': '1'
+        }
+        response = self.client.get(self.url, request_data)
+
+        self.assertEqual(STATUS_CODE__SUCCESS, response.status_code)
+
+        response_data = json.loads(response.content)
+
+        # Make sure expected keys in response.
+        EXPECTED_RESPONSE_KEYS = set([
+            VARIANT_LIST_RESPONSE_KEY__LIST,
+            VARIANT_LIST_RESPONSE_KEY__TOTAL,
+            VARIANT_LIST_RESPONSE_KEY__SET_LIST,
+            VARIANT_LIST_RESPONSE_KEY__KEY_MAP,
+        ])
+        self.assertEqual(EXPECTED_RESPONSE_KEYS, set(response_data.keys()),
+                "Missing keys %s\nGot keys %s" % (
+                        str(EXPECTED_RESPONSE_KEYS -
+                                set(response_data.keys())),
+                        str(set(response_data.keys()))))
+
+        self.assertEqual(TOTAL_NUM_VARIANTS,
+                response_data[VARIANT_LIST_RESPONSE_KEY__TOTAL])
+
+        # Check total variants returned is correct.
+        variant_data_obj = json.loads(response_data[
+                VARIANT_LIST_RESPONSE_KEY__LIST])
+        variant_obj_list = variant_data_obj['obj_list']
+        self.assertTrue(TOTAL_NUM_VARIANTS, len(variant_obj_list))
+
+        # Check positions are correct.
+        def _get_position_from_frontend_object(fe_obj):
+            return int(re.match('([0-9]+)', str(fe_obj[
+                    MELTED_SCHEMA_KEY__POSITION])).group(1))
+        variant_position_set = set([_get_position_from_frontend_object(obj)
+                for obj in variant_obj_list])
+        self.assertEqual(set(range(TOTAL_NUM_VARIANTS)), variant_position_set)
 
     def test_does_not_throw_500_on_server_error(self):
         """For user input errors, get_variant_list should not throw a 500 error.