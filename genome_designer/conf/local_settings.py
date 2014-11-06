ff3fbccf0f480076e9bc9fa41ce13314d39a118e
Daniel Bryan Goodman <dbgoodman@users.noreply.github.com>
2014-11-06 12:33:26 -0500

    Re-comment local-settings.py fix for OSX.

--- genome_designer/conf/local_settings.py
+++ genome_designer/conf/local_settings.py
@@ -3,14 +3,14 @@
 
 # Uncomment the following if running Postgres under different username
 # (i.e. for Postgres.app on OSX)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gdv2db',
-        'USER': 'gdv2dbuser',
-        'PASSWORD': 'g3n3d3z',
-        'HOST': 'localhost',
-        'PORT': '',
-        'OS_USER': 'YOUR_USERNAME'
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'millstone_user',
+#        'USER': 'millstone_user',
+#        'PASSWORD': 'millstone_pwd',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#        'OS_USER': 'YOUR_USERNAME'
+#    }
+#}
