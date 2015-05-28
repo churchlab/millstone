7310068cbaa8aa5e98f21bcee117210f4a00a193
glebk <gleb.kuznetsov@gmail.com>
2015-05-28 10:34:15 -0400

    Reproducing failing Celery topology w/ simple task.

---
+++ genome_designer/debug/2015_05_27_celery_canvas_debug.py
@@ -0,0 +1,74 @@
+"""Celery failure case.
+"""
+
+from celery import Celery
+from celery import group
+
+app = Celery('2015_05_27_celery_canvas_debug')
+app.config_from_object('debug_celeryconfig')
+
+
+@app.task
+def t(v):
+    print v
+    return v
+
+
+if __name__ == '__main__':
+
+    ### From Github issue
+
+    # g = group(
+    #     t.si(1),
+    #     chain(
+    #         t.si(2),
+    #         t.si(3)
+    #     )
+    # )
+
+    # print 'Now get results'
+    # print chain(g).delay().get()  # this works
+    # # chain(g, t.si(4)).delay().get()  # this works
+    # # chain(t.si(0), g).delay().get()  # this fails
+
+    ### Reproducing Millstone bug.
+
+    # This works.
+    group_1 = group([
+        t.si('group_1_task_1'),
+        t.si('group_1_task_2'),
+    ])
+    group_2 = group([
+        t.si('group_2_task_1'),
+    ])
+    addtl_task_1 = t.si('addtl_task_1')
+    addtl_task_2 = t.si('addtl_task_2')
+    pipeline = group_1 | group_2 | addtl_task_1 | addtl_task_2
+    assert pipeline.delay().get() == 'addtl_task_2'
+
+    # This fails.
+    group_1 = group([
+        t.si('group_1_task_1'),
+    ])
+    group_2 = group([
+        t.si('group_2_task_1'),
+        t.si('group_2_task_2'),
+    ])
+    addtl_task_1 = t.si('addtl_task_1')
+    addtl_task_2 = t.si('addtl_task_2')
+    pipeline = group_1 | group_2 | addtl_task_1 | addtl_task_2
+    assert pipeline.delay().get() == 'addtl_task_2'
+
+    # This also fails.
+    group_1 = group([
+        t.si('group_1_task_1'),
+        t.si('group_1_task_2')
+    ])
+    group_2 = group([
+        t.si('group_2_task_1'),
+        t.si('group_2_task_2') # NEW
+    ])
+    addtl_task_1 = t.si('addtl_task_1')
+    addtl_task_2 = t.si('addtl_task_2')
+    pipeline = group_1 | group_2 | addtl_task_1 | addtl_task_2
+    assert pipeline.delay().get() == 'addtl_task_2'
---
+++ genome_designer/debug/debug_celeryconfig.py
@@ -0,0 +1,8 @@
+BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
+
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_ACCEPT_CONTENT=['json']
+# CELERY_TIMEZONE = 'Europe/Oslo'
+# CELERY_ENABLE_UTC = True
