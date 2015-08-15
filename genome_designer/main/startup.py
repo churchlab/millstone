efd0a50e905d3dc2ab6a7f8563e5f4b7b65c0cb2
glebk <gleb.kuznetsov@gmail.com>
2015-08-15 16:57:01 -0400

    Code that checks that any migrations have been applied.

--- genome_designer/main/startup.py
+++ genome_designer/main/startup.py
@@ -3,12 +3,15 @@
 
 from django.db import connection
 from django.db import transaction
+from south.migration import Migrations
+from south.models import MigrationHistory
 
 
 def run():
     """Call this from manage.py or tests.
     """
     _add_custom_mult_agg_function()
+    _check_migrations_applied()
 
 
 def _add_custom_mult_agg_function():
@@ -35,3 +38,17 @@ def _add_custom_mult_agg_function():
                 ');'
         )
         transaction.commit_unless_managed()
+
+
+def _check_migrations_applied():
+    """Checks that all south migrations have been applied.
+    """
+    APP_NAME = 'main'
+    all_migrations = Migrations(APP_NAME)
+    applied_migrations = [migration.get_migration() for migration in
+            MigrationHistory.objects.filter(app_name=APP_NAME)]
+    not_applied = set(all_migrations) - set(applied_migrations)
+    if len(not_applied):
+        raise AssertionError(
+                "Database migration required. "
+                "Please run `./manage.py migrate main`")
