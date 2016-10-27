e1643c4a876437b55b473b4f00465f86a1612e94
Gleb Kuznetsov <gleb.kuznetsov@gmail.com>
2016-10-27 04:05:09 +0000

    Updates to script for generating postgresql indexing command.

--- genome_designer/debug/2016_10_06_test_create_postgresql_indeces.py
+++ genome_designer/debug/2016_10_06_test_create_postgresql_indeces.py
@@ -22,9 +22,55 @@ cols = [
     'es_data',
 ]
 
+json_keys = {
+        'va_data': [
+                'INFO_AF',
+                'INFO_AO',
+                'INFO_DPRA',
+                'INFO_EFF_CLASS',
+                'INFO_EFF_CODING',
+                'INFO_EFF_EFFECT',
+                'INFO_EFF_GENE',
+                'INFO_EFF_IMPACT',
+                'INFO_LEN',
+                'INFO_SV_LEN',
+                'INFO_TYPE'
+        ],
+
+        'vccd_data': [
+            'INFO_DP',
+            'INFO_METHOD',
+            'INFO_NS',
+            'INFO_SV_TYPE',
+            'IS_SV',
+        ],
+
+        've_data': [
+                'AF',
+                'DP',
+                'GT_TYPE',
+                'IS_HET',
+                'RO',
+                'AO',
+        ],
+
+        'es_data': [
+                'SAMPLE_FITNESS',
+                'SAMPLE_LINE',
+                'SAMPLE_PARENTS'
+        ],
+}
+
 with open('millstone_create_indeces_cmd.txt', 'w') as fh:
-    for c in cols:
-        fh.write(
-            'CREATE INDEX mmv_{ref_uid}_{col} on materialized_melted_variant_{ref_uid} ({col});\n'.format(
-                ref_uid=REF_GENOME_UID,
-                col=c))
+     for c in cols:
+         fh.write(
+             'CREATE INDEX mmv_{ref_uid}_{col} on materialized_melted_variant_{ref_uid} ({col});\n'.format(
+                 ref_uid=REF_GENOME_UID,
+                 col=c))
+     for data_key, subkey_list in json_keys.iteritems():
+         for subkey in subkey_list:
+            fh.write(
+                'CREATE INDEX mmv_{ref_uid}_{data_key}_{subkey} on materialized_melted_variant_{ref_uid}(({data_key}->>\'{subkey}\'));\n'.format(
+                    ref_uid=REF_GENOME_UID,
+                    data_key=data_key,
+                    subkey=subkey))
